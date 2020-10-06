720202d170a6220458f28ea7329e8df3b3af7669
Sanskar Jain <sanskarjain1997.19@gmail.com>
2020-10-07 00:14:36 +0530

    First Upload

---
+++ exp.py
@@ -0,0 +1,64 @@
+from scipy.io.wavfile import write
+import numpy as np
+
+samplerate = 44100
+
+def get_piano_notes():
+    '''
+    Returns a dict object for all the piano 
+    note's frequencies
+    '''
+    # White keys are in Uppercase and black keys (sharps) are in lowercase
+    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
+    base_freq = 261.63 #Frequency of Note C4
+    
+    note_freqs = {octave[i]: base_freq * pow(2,(i/12)) for i in range(len(octave))}        
+    note_freqs[''] = 0.0
+    
+    return note_freqs
+    
+def get_wave(freq, duration=0.5):
+    global wave_delta_arcsin
+    #length = int(duration * samplerate)
+    amplitude = 4096
+    t = np.linspace(0, duration, int(samplerate * duration))
+    wave = amplitude * np.sin(2 * np.pi * freq * t)
+    
+    return wave
+    
+    
+def get_song_data(music_notes):
+    note_freqs = get_piano_notes()
+    song = [get_wave(note_freqs[note]) for note in music_notes.split('-')]
+    song = np.concatenate(song)
+    return song.astype(np.int16)
+    
+def get_chord_data(chords):
+    chords = chords.split('-')
+    note_freqs = get_piano_notes()
+    
+    chord_data = []
+    for chord in chords:
+        data = sum([get_wave(note_freqs[note]) for note in list(chord)])
+        chord_data.append(data)
+    
+    chord_data = np.concatenate(chord_data, axis=0)    
+    return chord_data.astype(np.int16)
+
+
+def main():
+    #Notes of "twinkle twinkle little star"
+    music_notes = 'C-C-G-G-A-A-G--F-F-E-E-D-D-C--G-G-F-F-E-E-D--G-G-F-F-E-E-D--C-C-G-G-A-A-G--F-F-E-E-D-D-C'
+    data = get_song_data(music_notes)
+    data = data * (16300/np.max(data))
+    write('exp.wav', samplerate, data.astype(np.int16))
+    
+    #Playing chords
+    chords = 'EgB-DfA-AcE-BDf-gAcE-fAc'
+    data = get_chord_data(chords)
+    data = data * (16300/np.max(data))
+    data = np.resize(data, (len(data)*5,))
+    write('exp-C-Major.wav', samplerate, data.astype(np.int16))
+    
+if __name__=='__main__':
+    main()
